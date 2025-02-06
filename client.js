6498b870b0455f9a05c7d34cbb117f6cd67b756f
Saph69 <128137634+Saph69@users.noreply.github.com>
2025-02-06 19:08:47 +0100

    Update client.js

--- client.js
+++ client.js
@@ -52,8 +52,8 @@ async function fetchEpisodes(page = 1, itemsPerPage = 10) { // Increased items p
         if (loadingSpinner) loadingSpinner.style.display = 'block';
         if (errorContainer) errorContainer.style.display = 'none';
         
-        const functionUrl = 'https://func-website-backend.azurewebsites.net/api/HttpTrigger1';
-        const functionKey = "3teAYWB1X3ArvHMD7_XypbjgEpk7Lo4VZBZzfZ2Pgd2GAzFu94tslg=="
+        const functionUrl = '/api/HttpTrigger1';
+        //const functionKey = "3teAYWB1X3ArvHMD7_XypbjgEpk7Lo4VZBZzfZ2Pgd2GAzFu94tslg=="
         
         console.log('Making API request...');
         const response = await fetch(`${functionUrl}?code=${functionKey}`);
